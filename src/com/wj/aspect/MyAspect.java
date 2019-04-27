5ddcc4c88018674e80f59cad0a278dee7a2311e4
MoonMoonM <729232064@qq.com>
2019-04-27 20:39:55 +0800

    Spring Aop JDK动态代理

---
+++ src/com/wj/aspect/MyAspect.java
@@ -0,0 +1,11 @@
+package com.wj.aspect;
+
+public class MyAspect {
+    //切面类：可以存在多个通知Advice
+    public void check_Permissions(){
+        System.out.println("模拟检查权限...");
+    }
+    public void log(){
+        System.out.println("模拟记录日志...");
+    }
+}
---
+++ src/com/wj/dao/UserDao.java
@@ -0,0 +1,6 @@
+package com.wj.dao;
+
+public interface UserDao {
+    public void addUser();
+    public void deleteUser();
+}
